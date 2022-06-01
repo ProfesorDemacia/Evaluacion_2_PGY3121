f49e80c73d57e3c3dfeb8ec6c0af1bb00719ef5d
ProfesorDemacia <ce.contreras@duocuc.cl>
2022-06-01 00:02:48 -0400

    ultimo del dia

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-function validar(){
+function validacion(){
 
-    var nombre = document.formulario.txt_nombre.value
-    var apellido = document.formulario.txt_apellido.value
-    var rut = document.formulario.txt_rut.value
-    var telefono = documente.formulario.txt_numero.value
-    var correo = document.formulario.txt_correo.value
+    var nombre = document.getElementById(txt_nombre).value
+    var apellido = document.getElementById(txt_apellido).value
+    var rut = document.getElementById(txt_rut).value
+    var telefono = documente.form_contact.txt_numero.value
+    var correo = document.form_contact.txt_correo.value
     var region = document.getElementById("sel_region")
 
 
     if (rut.length>10 || rut.length<9)
     {
         alert("El rut no es valido... Tiene que tener entre 9 y 10 caracteres")
-        document.formulario.txt_rut.focus()
+        document.form_contact.txt_rut.focus()
+        return false
     }
     if (nombre.length < 3)
     {
         alert("El nombre tiene que ser mayor a 3 caracteres")
-        document.formulario.txt_nombre.focus()
+        document.form_contact.txt_nombre.focus()
         return false
     }
     if (apellido.length <3)
     {
         alert("El apellido tiene que tener mas de 3 caracteres")
-        document.formulario.txt_apellido.focus()
+        document.form_contact.txt_apellido.focus()
         return false
     }
     if (correo.length <3)
     {
         alert("El correo electronico tiene que tener mas de 3 caracteres")
-        document.formulario.txt_correo.focus()
+        document.form_contact.txt_correo.focus()
         return false
     }
     if (correo.indexOf('@')<0 || correo.indexOf('.')<0)
     {
         alert("Debe ingresar un correo  valido ej. nombre@empresa.com")
-        document.formulario.txt_correo.focus()
+        document.form_contact.txt_correo.focus()
         return false
 
     }
-    if ()
+    document.write(nombre)
 
 }
